fa20f82423d78f1c4728bc6ca05aee93bdc1dcb8
Sean Tate <state@spindle.com>
2015-11-03 14:11:17 -0800

    Updated javascript for 4.0 compatability.

--- app/inputs/search_select_tags_input.rb
+++ app/inputs/search_select_tags_input.rb
@@ -5,6 +5,7 @@ class SearchSelectTagsInput < Formtastic::Inputs::SelectInput
     opts["data-fields"] = (@options[:fields] || []).to_json
     opts["data-url"] = @options[:url] || ""
     opts["data-minimum_input_length"] = @options[:minimum_input_length] || 1
+    opts["data-display_name"] = @options[:display_name] || "name"
     super.merge opts
   end
 end
