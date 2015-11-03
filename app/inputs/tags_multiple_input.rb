d2adab43195c2061a4cd494d444732284686ffb9
Sean Tate <state@spindle.com>
2015-11-03 10:45:20 -0800

    Updated version of select2.

--- app/inputs/tags_multiple_input.rb
+++ app/inputs/tags_multiple_input.rb
@@ -5,7 +5,6 @@ class TagsMultipleInput < Formtastic::Inputs::SelectInput
     opts["data-fields"] = (@options[:fields] || []).to_json
     opts["data-url"] = @options[:url] || ""
     opts["data-minimum_input_length"] = @options[:minimum_input_length] || 1
-    opts["data-collection"] = (@options[:collection] || []).to_json
     super.merge opts
   end
 end
